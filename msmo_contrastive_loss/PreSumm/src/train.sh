44f66e89c9dbdc70fe3c4032b0f69f54032f581c
MMSumm <94446054+MMSumm@users.noreply.github.com>
2021-11-25 22:10:47 +0530

    Update train.sh

--- msmo_contrastive_loss/PreSumm/src/train.sh
+++ msmo_contrastive_loss/PreSumm/src/train.sh
@@ -1,16 +1,5 @@
 #!/bin/sh
-#SBATCH --job-name=bert_section
-#SBATCH -A research
-#SBATCH -c 30
-#SBATCH -o bert_section.out
-#SBATCH --gres=gpu:4
-#SBATCH --mem-per-cpu=3000M
-#SBATCH --time=4-00:00:00
-#SBATCH --mail-type=END
-#SBATCH --mail-user=anshul.padhi@research.iiit.ac.in
-#SBATCH -w gnode33
 
-# # # old = lr_dec = 0.2, lr_bert = 0.002, warmup steps bert 20k warmup steps dec 10k
 
 python train.py  -task abs -mode train -bert_data_path /scratch/full_data_pt/ -dec_dropout 0.2 \
 -model_path /scratch/new_msmo -sep_optim true -lr_dec 0.02 -save_checkpoint_steps \
@@ -18,4 +7,3 @@ python train.py  -task abs -mode train -bert_data_path /scratch/full_data_pt/ -d
 -use_interval true -warmup_steps_bert 16000 -warmup_steps_dec 8000 -max_pos 512 -visible_gpus \
 0,1,2,3 -log_file log
 
-#### CHANGED GPUS REMEMBER GANDU, REDUCED BATCH SIZE TOO
